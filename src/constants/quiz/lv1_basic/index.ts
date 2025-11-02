6d58f1de35d071ac46695d1a998dd9e13fd4e5a4
Jiyoon <SAMSUNG@Jiyoons-MacBook-Air.local>
2025-11-02 16:42:01 +0900

    feat: 퀴즈 타입 추가

---
+++ src/constants/quiz/lv1_basic/index.ts
@@ -0,0 +1,15 @@
+import { LV1_STOCK_INTRO } from "./1_stock_intro";
+import { LV1_MARKET_CAP } from "./2_market_cap";
+import { LV1_MARKET_INDEX } from "./3_market_index";
+import { LV1_MARKET_INDEX_PART1 } from "./4_market_index_part1";
+import { LV1_MARKET_INDEX_PART2 } from "./5_market_index_part2";
+import { LV1_BOND_INTRO } from "./6_bond_intro";
+
+export const LV1_QUIZZES = [
+  ...LV1_STOCK_INTRO,
+  ...LV1_MARKET_CAP,
+  ...LV1_MARKET_INDEX,
+  ...LV1_MARKET_INDEX_PART1,
+  ...LV1_MARKET_INDEX_PART2,
+  ...LV1_BOND_INTRO,
+];
